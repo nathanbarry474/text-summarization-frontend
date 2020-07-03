2857960ac02e3ba3289b3c698f04e654a2b22831
Nathan Barry <Nathanbarry474@gmail.com>
2020-07-03 14:09:08 -0500

    added basic functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 function App() {
+  const [summary, setSummary] = useState(null);
+  const [text, setText] = useState(null);
+
+  const textSelectedHandler = (e) => {
+    setText(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const textUploadHandler = () => {
+    let fd = new FormData();
+    fd.append("text", text);
+    axios
+      .post("http://127.0.0.1:5000/", fd)
+      .then((res) => {
+        setSummary(res.data.result[0].summary_text);
+        console.log(res);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Text Summarizer</h2>
+      <textarea
+        className="input"
+        onChange={textSelectedHandler}
+        placeholder="Enter Text You Want Summarized"
+      />
+      <button onClick={textUploadHandler}>Submit</button>
+      <h2>{summary}</h2>
     </div>
   );
 }
