f6d241a3c0245e95e5a67d2e05acb8f2b169214f
Nathan Barry <Nathanbarry474@gmail.com>
2020-07-04 20:44:45 -0500

    Made it as pretty as possible

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { useState } from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 import axios from "axios";
+import { Form, Button } from "react-bootstrap";
 
-const Page = styled.div`
+const Title = styled.h1`
+  padding-top: 10%;
+  padding-bottom: 50px;
+  font-size: 3rem;
+  margin: auto;
   text-align: center;
 `;
 
+const Inputs = styled.div`
+  width: 60%;
+  margin: 0 auto;
+`;
+
+const SumHeader = styled.h2`
+  padding-top: 60px;
+  padding-bottom: 20px;
+  text-align: center;
+`;
+
+const SummaryText = styled.p`
+  font-size: 1.5rem;
+  margin-bottom: 100px;
+`;
+
+const TextForm = styled(Form.Control)`
+  width: 100%;
+  height: 300px;
+  margin-bottom: 20px;
+`;
 
 function App() {
   const [summary, setSummary] = useState(null);
@@ -29,16 +55,23 @@ function App() {
   };
 
   return (
-    <Page>
-      <h2>Text Summarizer</h2>
-      <textarea
-        className="input"
-        onChange={textSelectedHandler}
-        placeholder="Enter Text You Want Summarized"
-      />
-      <button onClick={textUploadHandler}>Submit</button>
-      <h2>{summary}</h2>
-    </Page>
+    <React.Fragment>
+      <Title>Text Summarizer</Title>
+      <Inputs>
+        <TextForm
+          as="textarea"
+          onChange={textSelectedHandler}
+          placeholder="Enter Text You Want Summarized"
+        />
+        <Button variant="outline-dark" onClick={textUploadHandler}>
+          Let's Summarize!
+        </Button>
+        {
+        summary ? <SumHeader>Summary</SumHeader> : null
+        }
+        <SummaryText>{summary}</SummaryText>
+      </Inputs>
+    </React.Fragment>
   );
 }
 
